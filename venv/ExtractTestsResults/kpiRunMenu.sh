ea31648dd43ea1922c7113a60074bdfb53fc3544
Aviv Lugasi <alugasi@redhat.com>
2023-03-06 20:22:54 +0200

    updated kpi menu script

--- venv/ExtractTestsResults/kpiRunMenu.sh
+++ venv/ExtractTestsResults/kpiRunMenu.sh
@@ -4,7 +4,7 @@
 # menu for running test results extraction and storing them as json files automation script. #
 ##############################################################################################
 
-PATH_TO_JSON_FOLDERS=$(pwd)
+PATH_TO_JSON_FOLDERS="$(pwd)/JsonTestsFolders"
 
 function menu() {
    options=("PTP" "RFC" "CYCLICTEST" "OSLAT" "CPU UTIL" "REBOOT" "DEPLYOMENT" "QUIT")
@@ -17,50 +17,48 @@ function menu() {
             "PTP")
                 echo "You selected PTP"
                 read -p "enter PTP test output path: " path
-                read -p "enter PTP test Kernel: " kernel
                 read -p "enter PTP test config: " config_PTP
-                source ./extractResultsFromNetwork.sh 0 $path $kernel "$config_PTP"
+                source ./CreateNetwork/extractResultsFromNetwork.sh 0 $path "$config_PTP"
                 break
                 ;;
             "RFC")
                 echo "You selected RFC"
                 read -p "enter RFC test output path: " path
-                read -p "enter RFC test Kernel: " kernel
                 read -p "enter RFC test nic: " nic_RFC
-                source ./extractResultsFromNetwork.sh 1 $path $kernel $nic_RFC
+                source ./CreateNetwork/extractResultsFromNetwork.sh 1 $path $nic_RFC
                 break
                 ;;
             "CYCLICTEST")
                 echo "You selected CYCLICTEST"
                 read -p "enter CYCLICTEST output path: " path
                 read -p "enter CYCLICTEST operator version: " operator_version
-                source ./extractResultsFromAvailability.sh 1 $path $operator_version
+                source ./CreateAvailability/extractResultsFromAvailability.sh 1 $path $operator_version
                 break
                 ;;
             "OSLAT")
                 echo "You selected OSLAT"
                 read -p "enter OSLAT test output path: " path
                 read -p "enter OSLAT test operator version: " operator_version
-                source ./extractResultsFromAvailability.sh 0 $path $operator_version
+                source ./CreateAvailability/extractResultsFromAvailability.sh 0 $path $operator_version
                 break
                 ;;
             "CPU UTIL")
                 echo "You selected CPU UTIL"
                 read -p "enter CPU UTIL test output path: " path
-                source ./extractResultsFromCPUUtil.sh $path
+                source ./CreateCpuUtil/extractResultsFromCPUUtil.sh $path
                 break
                 ;;
             "REBOOT")
                 echo "You selected REBOOT"
                 read -p "enter REBOOT test output path: " path
                 read -p "enter REBOOT test operator version: " operator_version
-                source ./extractResultsFromReboot.sh $path $operator_version
+                source ./CreateReboot/extractResultsFromReboot.sh $path $operator_version
                 break
                 ;;
             "DEPLYOMENT")
                 echo "You selected DEPLYOMENT"
                 read -p "enter DEPLYOMENT test output path: " path
-                source ./ExtractResultsFromDeployment.sh $path
+                source ./CreateDeployment/ExtractResultsFromDeployment.sh $path
                 break
                 ;;
             "QUIT")
